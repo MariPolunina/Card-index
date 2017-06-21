044809878e5091ec62747256704d465114cbebc2
Марина <www.marina.007@mail.ru>
2017-06-21 21:57:00 +0300

    Первое окно

---
+++ Library/Library/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using GalaSoft.MvvmLight;
+using Library.Model;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Library.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private string find;
+        public string _find
+        {
+            get
+            {
+                return find;
+            }
+            set
+            {
+                find = value;
+            }
+        }
+       public List<string> Filter{ get; set; }
+        public string SelectedFilter { get; set; }
+
+        public List<string> Menu { get; set; }
+        public string SelectedMenu { get; set; }
+        public ObservableCollection<string> Elements { get; set; }
+        public FindCommand F { get; set; }
+        public MainViewModel()
+        {
+            Filter = new List<string>()
+            {
+                "по книге",
+                "по автору"
+            };
+            F = new FindCommand();
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
